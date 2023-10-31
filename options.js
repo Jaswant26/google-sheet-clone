701dae017be034279bac4793e3defffe54643b6f
jaswant26 <jaswantsinghrawat3@gmail.com>
2023-10-31 14:01:15 +0530

    google sheet clone

---
+++ options.js
@@ -0,0 +1,17 @@
+const cellNamePlaceholder =document.querySelector("#active-cell");
+let activeElement = null;
+function onCellFocus(event) {
+    const elementId = event.target.id;
+    cellNamePlaceholder.innerText = elementId;
+    activeElement = event.target;
+    if (state[elementId]) {
+        // already selected cell
+        // fill the options with the state of that cell
+        resetOptions(state[elementId]);
+    }
+    else {
+        // selected for the first time
+        // fill the options with default state
+        resetOptions(defaultProperties);
+    }
+}
